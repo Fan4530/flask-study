d314f4195ab486f696bfd2bc9ed751b69efb99c6
russomp <russomp@usc.edu>
2018-10-16 23:00:40 -0400

    feat: Declare user model and schemas [DIST-143]

--- api/models/user.py
+++ api/models/user.py
@@ -1,10 +1,20 @@
+from datetime import datetime
+
 from marshmallow_mongoengine import ModelSchema
 
-from ..extensions import db
+from ..extensions import db, ma
 
 
 class User(db.Document):
-  pass 
+  public_id = db.UUIDField(required=True, unique=True)
+  email = db.EmailField(required=True, unique=True)
+  password = db.StringField(max_length=80, required=True)
+  first_name = db.StringField(max_length=50)
+  last_name = db.StringField(max_length=50)
+  role = db.StringField(default="writer", validation=lambda role: role in ["writer", "admin"])
+  created_at = db.DateTimeField(default=datetime.utcnow)
+
+  meta = {'collection': 'users'}
 
 
 class UserSchema(ModelSchema):
@@ -12,5 +22,5 @@ class UserSchema(ModelSchema):
         model = User
 
 
-user_schema = UserSchema()
+user_schema = UserSchema(load_only=("id", "password", "created_at"))
 users_schema = UserSchema(many=True)
